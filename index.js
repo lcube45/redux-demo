b33fb7e89827a03ac756b1f58e24b0c1134d786a
Laurent LE LARDEUX <laurent.le-lardeux@inetum.com>
2022-09-05 10:45:50 +0200

    redux basic pattern

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log('from index.js')
+const redux = require('redux')
+const createStore = redux.createStore
+
+const CAKE_ORDERED = 'CAKE_ORDERED'
+
+// action
+function orderCake() {
+    return {
+        type: CAKE_ORDERED,
+        quantity: 1
+    }
+}
+
+const initialState = {
+    numOfCakes: 10,
+}
+
+// reducer
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case 'CAKE_ORDERED':
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes - 1
+            }
+        default:
+            return state
+    }
+}
+
+// store
+const store = createStore(reducer)
+console.log('initial state', store.getState())
+
+// subscribe/unsubscribe
+const unbsubscribe = store.subscribe(() => 
+    console.log('Update state', store.getState())
+)
+
+// dispatch action
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+
+unbsubscribe()
